070a571160b8916b63766ecbe075e945cb175a29
imgomi <webmaster@mail.gomi.land>
2021-04-17 20:15:45 +0900

    💬 덧글 CRUD

--- api/src/articles/resolvers/comments/comments.resolver.ts
+++ api/src/articles/resolvers/comments/comments.resolver.ts
@@ -1,11 +1,30 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateCommentDto } from 'src/articles/dtos/commentDtos/create-comment.dto';
+import { UpdateCommentDto } from 'src/articles/dtos/commentDtos/update-comment.dto';
 import { Comments } from 'src/articles/entities/comments.entity';
 import { CommentsService } from 'src/articles/services/comments/comments.service';
 
 @Resolver()
 export class CommentsResolver {
   constructor(private readonly commentsService: CommentsService) {}
+
+  @Query(() => [Comments])
+  async comments(@Args('articleId') articleId: number) {
+    return this.commentsService.findCommentsByArticleId(articleId);
+  }
+
+  @Mutation(() => Boolean)
+  async deleteComments(@Args('commentId') commentId: number) {
+    return this.commentsService.deleteComment(commentId);
+  }
+
+  @Mutation(() => Comments)
+  async updateComment(
+    @Args('commentId') commentId: number,
+    @Args('comment') updateCommentDto: UpdateCommentDto,
+  ) {
+    return this.commentsService.updateComment(commentId, updateCommentDto);
+  }
 
   @Mutation(() => Comments)
   async newComment(
--- api/src/articles/services/comments/comments.service.ts
+++ api/src/articles/services/comments/comments.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateCommentDto } from 'src/articles/dtos/commentDtos/create-comment.dto';
+import { UpdateCommentDto } from 'src/articles/dtos/commentDtos/update-comment.dto';
 import { Comments } from 'src/articles/entities/comments.entity';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { ArticlesService } from '../articles/articles.service';
 
 @Injectable()
@@ -12,6 +13,29 @@ export class CommentsService {
     private readonly commentsRepository: Repository<Comments>,
     private readonly articlesService: ArticlesService,
   ) {}
+
+  async findCommentsByArticleId(articleId: number): Promise<Comments[]> {
+    return this.commentsRepository.find({
+      article: {
+        id: articleId,
+      },
+    });
+  }
+
+  async deleteComment(id: number): Promise<DeleteResult> {
+    return this.commentsRepository.delete({
+      id,
+    });
+  }
+
+  async updateComment(
+    id: number,
+    { content }: UpdateCommentDto,
+  ): Promise<UpdateResult> {
+    return this.commentsRepository.update(id, {
+      content,
+    });
+  }
 
   async createComment(
     articleId: number,
